57570bf3f4f553512808597c0c88f80045a55322
kumar <kumarmanikyrao@gmail.com>
2021-09-16 10:30:59 -0400

    Resolved backend merge conflict

--- Grocery-Store/Back-end/app.js
+++ Grocery-Store/Back-end/app.js
@@ -2,21 +2,13 @@ let express = require('express');
 let mongoose = require('mongoose');
 var bodyparser = require('body-parser');
 let app = express();
-<<<<<<< HEAD
 var productsRouter = require('./routes/product');
 let empRequestRouter = require('./routes/empRequest.route');
-
+let userRouter = require('./routes/userRouter');
 var adminRouter = require('./routes/admin');
-var employeeRouter = require('./routes/employeeRouter');
-=======
-var productsRouter = require("./routes/product");
-let empRequestRouter = require("./routes/empRequest.route");
-let userRouter = require("./routes/userRouter");
-var adminRouter = require("./routes/admin");
 //var employeeRouter = require("./routes/employeeRouter");
 
-let orderRouter = require("./routes/order")
->>>>>>> ba19adea708d08afd45cc8ba6f0eff271cc6f132
+let orderRouter = require('./routes/order');
 
 var cors = require('cors');
 app.use(bodyparser.json());
@@ -39,23 +31,12 @@ mongoose
 		console.log(error);
 	});
 
-<<<<<<< HEAD
 app.use('/products', productsRouter);
 app.use('/admin', adminRouter);
 app.use('/request', empRequestRouter);
-app.use('/employee', employeeRouter);
-
-app.get('/', (req, res) => {
-	res.send('Foobar');
-});
-=======
-app.use("/products", productsRouter);
-app.use("/admin", adminRouter);
-app.use("/request", empRequestRouter);
-app.use("/order", orderRouter);
+app.use('/order', orderRouter);
 //app.use("/employee", employeeRouter);
-app.use("/user",userRouter);
->>>>>>> ba19adea708d08afd45cc8ba6f0eff271cc6f132
+app.use('/user', userRouter);
 
 app.listen(5000, () => {
 	console.log('Listening on port 5000');
