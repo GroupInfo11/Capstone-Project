05459cd1dfdcc906c1384f63ffc835bfcc70ce6f
rkdudkey <rattikarn.dudley@gmail.com>
2021-09-16 14:35:58 -0400

    add update user account lock

--- Grocery-Store/Front-end/src/app/emp-update-order/emp-update-order.component.ts
+++ Grocery-Store/Front-end/src/app/emp-update-order/emp-update-order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {EmpOrderService} from '../services/emp-order.services'
+import {Order} from '../models/order';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-emp-update-order',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpUpdateOrderComponent implements OnInit {
 
-  constructor() { }
+  orders:Array<Order>=[];
+  flag:boolean = false;
+  //variable to update status from user input
+  customerEmail?:String;
+  orderStatus?:number;
+  updateMsg?:string;
+
+
+  tasks = [
+    { id: 1, name: "Shipped" },
+    { id: 2, name: "Out For Delivery" },
+    { id: 3, name: "Delivery" },
+    { id: 3, name: "Cancelled" },
+  ];
+
+  orderRef = new FormGroup({
+    customerEmail: new FormControl(),
+    operation: new FormControl()
+  })
+
+  
+  constructor(public empOrderSer:EmpOrderService) { }
 
   ngOnInit(): void {
+    this.getallOrders();
+  }
+
+  getallOrders(){
+    this.empOrderSer.retrieveAllOrderInfo().subscribe(result => {
+      this.orders = result;
+      console.log(this.orders);
+    }), error => console.log(error);
+  }
+
+  updateStatus(customerEmail:any, orderStatus:any){
+    this.updateMsg ="";
+    this.flag = true;
+    this.customerEmail = customerEmail;
+    this.orderStatus = orderStatus;
+  }
+
+  nowUpdateOrderStatus(){
+   let cusEmail = this.orderRef.value.customerEmail;
+   let updateOrder = this.orderRef.value.operation;
+
+   this.empOrderSer.updateCustomerOrderStatus(cusEmail, updateOrder).subscribe( result => {
+     
+     this.updateMsg = result.message;
+     this.flag = false;
+     this.getallOrders();
+     
+   }, err => console.log(err));
   }
 
 }
--- Grocery-Store/Front-end/src/app/models/order.ts
+++ Grocery-Store/Front-end/src/app/models/order.ts
@@ -1,15 +1,3 @@
-import { Product } from './product';
-
 export class Order {
-  _id:Number;
-  customerEmail:String;
-  Order:{
-    productName:String,
-    ProductPrice:String,
-    ProductId:String,
-    created_at: { type: Date, default: String },
-    orderID:Number;
-  };
-  totalPrice:Number;
-  orderStatus:String;
+ 
 }
