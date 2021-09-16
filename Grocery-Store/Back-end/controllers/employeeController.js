360ef2e887f7c3c2dbc46a8bb854a66e3ff753b5
Kevin Pecci <s1191114@monmouth.edu>
2021-09-16 14:16:54 -0400

    updated delete employee to work with backend

--- Grocery-Store/Back-end/controllers/employeeController.js
+++ Grocery-Store/Back-end/controllers/employeeController.js
@@ -14,8 +14,8 @@ let signIn = async(req,res)=>{
 
 let deleteEmp = async(req,res)=>{
     let deleteid = req.body.id;
-    let userInfo = await employeeModel.findOne({id:deleteid});
-    let info = await employeeModel.deleteOne({userInfo});
+    //let userInfo = await employeeModel.findOne({id:deleteid});
+    let userInfo = await employeeModel.deleteOne({user:deleteid});
     console.log(userInfo);
     if(userInfo!=null){
         res.send("Success");
